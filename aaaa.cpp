e5699b835f231a231631b55ff34ed9619ed11e0e
hjjobs <himanshujh46@gmail.com>
2023-11-04 12:02:44 +0530

    test

---
+++ aaaa.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define max(a,b) ((a)>(b)?(a):(b))
+#define min(a,b) ((a)<(b)?(a):(b))
+#define abs(x) ((x)<0?-(x):(x))
+#define pii pair<int,int>
+#define m_p make_pair(n,m)
+#define mod 1000000007
+#define pb push_back
+#define bp(x) __builtin_popcount(x)
+typedef long long int ll;
+int main()
+{
+	#ifndef ONLINE_JUDGE
+		freopen("input.txt","r",stdin);
+		freopen("output.txt","w",stdout);
+	#endif
+	ios::sync_with_stdio(false);cin.tie(0);
+	string a;
+	a[3]='A'+3;
+	cout<<a[3];
+	return 0;
+}
